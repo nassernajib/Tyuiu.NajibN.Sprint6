d944f556bf41e337f675fdca56a264e6f9bba75e
нассер <@NASSER>
2025-04-26 19:39:48 +0500

    Task1

---
+++ Tyuiu.NajibN.Sprint6.Task1.V14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NajibN.Sprint6.Task1.V14.Lib;
+using static System.Net.Mime.MediaTypeNames;
+namespace Tyuiu.NajibN.Sprint6.Task1.V14
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            object value = Application.EnableVisualStyles();
+            object value1 = Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain_LMV());
+        }
+      
+        }
+    }
+
